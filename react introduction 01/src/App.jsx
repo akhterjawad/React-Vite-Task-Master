ee1e5749826ecd1324f068eaa1dd60fbb1101ba6
Muhammad Jawad <akhterjawad566@gmail.com>
2024-08-22 00:15:38 +0500

    complete

---
+++ react introduction 01/src/App.jsx
@@ -0,0 +1,9 @@
+// react ka under jo bhi hum function bnainga wo 1 compnent ha
+// or coponet ka pahala letter captial hoga 
+
+function App(){
+    return (
+      <h1>Hello React World</h1>
+    )
+  }
+  export default App
