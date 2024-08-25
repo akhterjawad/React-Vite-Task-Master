c99d1fbd8c5d7aba13c011cf1868b116db5ce36e
Muhammad Jawad <akhterjawad566@gmail.com>
2024-08-25 17:30:09 +0500

    complete

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([]);
+  const inputValue = useRef()
+
+  let addTodo= event =>{
+    event.preventDefault()
+    const newTodo = inputValue.current.value;
+    if (newTodo) {
+      setTodo([...todo, newTodo]); // Properly updating the state
+      inputValue.current.value = ''; // Clear the input field
+    }else{
+      alert(`enter something first`)
+    }
+  }
+function deleteTodo(i) {
+  todo.splice(i,1)
+  setTodo([...todo])
+  console.log(i,`delete`);
+  console.log(todo);
+}
+function editTodo(i) {
+  let editTodoValue = prompt(`enter edit value`,todo[i])
+  todo.splice(i,1,editTodoValue)
+  setTodo([...todo])
+  console.log(i,`edit`);
+  console.log(todo);
+}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form onSubmit={addTodo} >
+      <input type="text" placeholder='add todo' ref={inputValue} />
+      <button  type='submit' onClick={addTodo}>add</button>
+    </form>
+<ul>
+  {/* key ma index dena is not a good practice */}
+  {todo.map((item , index)=>{
+        return <div key={index}>
+          <li >{item}</li>
+          <button onClick={()=> deleteTodo(index)}>delete</button>
+          <button onClick={()=> editTodo(index) }>edit</button>
+        </div>
+      })}
+</ul>
     </>
   )
 }
