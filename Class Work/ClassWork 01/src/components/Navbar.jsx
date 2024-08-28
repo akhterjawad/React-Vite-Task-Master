763d81b1016b057033dbacbcb06de117902ef71a
Muhammad Jawad <akhterjawad566@gmail.com>
2024-08-28 23:04:43 +0500

    class work

---
+++ Class Work/ClassWork 01/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+function Navbar() {
+    return(
+        <>
+        <h1 className="bg" style={{
+            textAlign: 'center'
+        }} >navbar</h1>
+        </>
+    )
+}
+
+export default Navbar
