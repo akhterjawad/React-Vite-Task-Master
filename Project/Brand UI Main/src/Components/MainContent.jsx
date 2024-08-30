42bb0ccd89a0f55870facd9e971c9f4032d1654d
Muhammad Jawad <akhterjawad566@gmail.com>
2024-08-31 01:31:33 +0500

    Brand UI complete

--- Project/Brand UI Main/src/Components/MainContent.jsx
+++ Project/Brand UI Main/src/Components/MainContent.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 import shoes from '../assets/shoe_image.png'
+import flipkart from '../assets/flipkart.png'
+import amazon from '../assets/amazon.png'
 
 const MainContent = () => {
     return (
         <>
             <main>
                 <div className='flex flex-wrap ml-10 justify-center mt-10 '><p className=' below-sm:text-[5rem] text-[4rem] font-black text-black leading-none'>YOUR FEET<br />DESERVE<br />THE BEST</p>
-                    <div className=''><img className='w-[450px] ml-9 mt-[5px] below-md:mt-14' src={shoes} alt="shoes-img" /></div>
+                    <div className=''><img className='below-sm-2:w-[450px] w-[80%] ml-9 mt-[5px] below-md:mt-14' src={shoes} alt="shoes-img" /></div>
                 </div>
-                <div className='below-md:absolute flex flex-col justify-center items-center bottom-[13%] right-[47%] static'>
+                <div className='below-md:absolute flex flex-col justify-center items-center bottom-[3%] right-[47%] static'>
                     <div className='font-semibold text-center below-md:text-start'>
                         <p>YOUR FEET DESERVE THE BEST CARE AND COMFORT EVERY DAY.</p>
                         <p>WHETHER YOU'RE WALKING, RUNNING, OR RELAXING, TREAT</p>
@@ -21,6 +23,10 @@ const MainContent = () => {
                         <button className='ml-8 border-[1px] border-black mt-3  font-semibold rounded p-1 pl-2 pr-2 text-[1.2rem]'>category</button>
                     </div>
                     <p className='self-center below-md:self-start mt-5 text-gray-700'>Also Available On</p>
+                    <div className="flex items-center self-center below-md:self-start space-x-4">
+                        <img src={flipkart} className='' alt="flipkart" />
+                        <img src={amazon} className='' alt="amazon" />
+                    </div>
                 </div>
             </main>
         </>
--- Project/Brand UI Main/tailwind.config.js
+++ Project/Brand UI Main/tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       screens: {
         'below-sm': {'min': '500px'},
+        'below-sm-2': '545px',
         'below-md': '975px', // Custom screen for below 'sm'
       },
     },
