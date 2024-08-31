df8759996005f455710c4f0a06f0889617c4b0b1
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-01 00:31:46 +0500

    Project initalize

---
+++ Daily-Practice/src/App.jsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from 'react'
+import './App.css'
+
+
+
+let App = () => {
+    let [todo, setTodo] = useState([])
+    let inputV = useRef()
+    function addTodo(event) {
+        event.preventDefault()
+        todo.push(inputV.current.value)
+        setTodo([...todo])
+        console.log(todo);
+        console.log(inputV.current.value);
+        inputV.current.value = ``
+    }
+    function deleteTodo(i) {
+        todo.splice(i, 1)
+        setTodo([...todo])
+    }
+    return (
+        <>
+            <form onSubmit={addTodo}>
+                <input type="text" ref={inputV} />
+                <button type='submit' >add</button>
+            </form>
+            <ul>
+                {todo.map((item, index) => (
+                    <div key={index}>
+                        <li>{item}</li>
+                        <button onClick={() => deleteTodo(index)}>delete</button>
+                        {/* <button onClick={edit}>edit</button> */}
+                    </div>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+
+export default App
