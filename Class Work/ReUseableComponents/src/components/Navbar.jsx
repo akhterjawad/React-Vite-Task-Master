7abdb91b290b21d1b83773b3e895c543d457198d
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-01 20:20:38 +0500

    Hooks

---
+++ Class Work/ReUseableComponents/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './navbar.css'
+
+function Navbar() {
+    return (
+        <>
+            <h1 className="bg" style={{
+                textAlign: 'center',
+                padding: '2rem'
+            }} >navbar</h1>
+        </>
+    )
+}
+
+
+export default Navbar
