d4aca4a41b9efa20fa867c7683ad16a2a0522298
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-05 20:20:22 +0500

    working

--- Daily-Practice/src/App.jsx
+++ Daily-Practice/src/App.jsx
@@ -37,7 +37,6 @@ import './App.css'
 //     )
 // }
 
-// export default App
 
 
 // const App = () => {
@@ -55,7 +54,6 @@ import './App.css'
 //         </>
 //     )
 // }
-// export default App
 
 // const App = () => {
 //   return (
@@ -64,33 +62,73 @@ import './App.css'
 //     </>
 //   )
 // }
-// export default App
+
+// const App = () => {
+//   let [count, setcount] = useState(0)
+//   function add() {
+
+//     // agar aap 1 dafama 2 bar setcount ka function run karna chata ha to nicha diagay tarika sa nahi hoga
+//     setcount(count + 1)
+//     setcount(count + 1)
+
+
+//     // agar aap 1 dafama 2 bar setcount ka function run karna chata ha to nicha diagay tarika sa hojayga but bad practice ha.
+//     setcount(count += 1)
+//     setcount(count += 1)
+
+
+//     // agar aap 1 dafama 2 bar setcount ka function run karna chata ha to nicha diagay tarika sa hojayga or ya good practice ha.
+//     setcount(prevVal => prevVal + 1);
+//     setcount(prevVal => prevVal + 1);
+//   }
+//   return (
+//     <>
+//       <button onClick={add}>added {count}</button>
+//     </>
+//   )
+// }
+
 
 import React from 'react'
 
 const App = () => {
-  let [count, setcount] = useState(0)
-  function add() {
-
-    // agar aap 1 dafama 2 bar setcount ka function run karna chata ha to nicha diagay tarika sa nahi hoga
-    setcount(count + 1)
-    setcount(count + 1)
-
-
-    // agar aap 1 dafama 2 bar setcount ka function run karna chata ha to nicha diagay tarika sa hojayga but bad practice ha.
-    setcount(count += 1)
-    setcount(count += 1)
-
-
-    // agar aap 1 dafama 2 bar setcount ka function run karna chata ha to nicha diagay tarika sa hojayga or ya good practice ha.
-    setcount(prevVal => prevVal + 1);
-    setcount(prevVal => prevVal + 1);
+  let [todo, setTodo] = useState([])
+  let inputVal = useRef()
+  function Add(event) {
+    event.preventDefault()
+    console.log(inputVal.current.value);
+    // todo.push(inputVal.current.value)
+    setTodo([...todo, inputVal.current.value])
+    inputVal.current.value = ``
+  }
+  function deleted(i) {
+    todo.splice(i, 1)
+    setTodo([...todo])
+  }
+  function edit(i) {
+    let updateVal = prompt(`.`)
+    todo.splice(i, 1, updateVal)
+    setTodo([...todo])
   }
   return (
     <>
-      <button onClick={add}>added {count}</button>
+      <form onSubmit={Add}>
+        <input type="text" ref={inputVal} />
+        <button>addTodo</button>
+      </form>
+      <ul>
+        {todo.map((item, index) => (
+          <li key={index}>{item}
+            <div>
+              <button onClick={() => deleted(index)}>deleted</button>
+              <button onClick={() => edit(index)}>edit</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
+
 
 export default App
--- React-Project/Weather-App/src/App.jsx
+++ React-Project/Weather-App/src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useRef, useState } from 'react'
 
 const App = () => {
-  return (
-    <>
-      
-    </>
-  )
+
+    const [addweather, setaddweather] = useState(null)
+    let inputRef = useRef()
+
+    useEffect(() => {
+
+    }, [checkStatus])
+
+    function checkStatus(event) {
+        event.preventDefault()
+        axios(`https://api.weatherapi.com/v1/current.json?key=e3e98122324b454b92f44333241406&q=${inputRef.current.value}&aqi=no`)
+            .then(responce => {
+                console.log(responce.data);
+                setaddweather(responce.data)
+                // console.log(addweather);
+            }).catch(error => {
+                console.log(error);
+            })
+    }
+
+    return (
+        <>
+            <h1 className='text-center mt-5  font-semibold text-[2.5rem]'>Weather App</h1>
+            <form className='flex gap-3 justify-center items-center mt-2' onSubmit={checkStatus}>
+                <input type="text" className="input input-bordered input-accent w-full max-w-xs" placeholder='Enter city name!' ref={inputRef} />
+                <button className="btn hover:bg-[green] border-none  bg-[#23ff23]">Success</button>
+            </form>
+
+            <div>
+                {addweather && addweather.forecast && addweather.forecast.forecastday ? (
+                    addweather.forecast.forecastday.map((item, index) => (
+                        <div key={index}>
+                            <h1>Date: {item.date}</h1>
+                            <h1>Max Temp: {item.day.maxtemp_c}°C</h1>
+                            <hr />
+                        </div>
+                    ))
+                ) : (
+                    'loading'
+                )}
+            </div>
+
+        </>
+    )
 }
 
 export default App
