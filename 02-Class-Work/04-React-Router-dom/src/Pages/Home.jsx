28d871bf6743d92865418b20682278b10c27ffe1
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-09 10:17:48 +0500

    Routing

--- 02-Class-Work/04-React-Router-dom/src/Pages/Home.jsx
+++ 02-Class-Work/04-React-Router-dom/src/Pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
-export default Home
+  const [data, setData] = useState(null);
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      setData(json)
+    }).catch(err => console.log(err));
+
+
+  return (
+    <>
+      {data ? data.map((item) => {
+        return <div key={item.id}>
+          <p>{item.name}</p>
+        </div>
+      }) : <h1>Loading...</h1>}
+    </>
+  );
+};
+
+export default Home;
