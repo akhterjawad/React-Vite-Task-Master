33c4592aee4656370cfc203a76345de9c13327ea
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-10 20:30:57 +0500

    Complete

--- 01-React-Project/Single-Page-App/src/Pages/Home.jsx
+++ 01-React-Project/Single-Page-App/src/Pages/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
                                     href="#"
                                     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => singleUser(item)}
                                 >
-                                    Read more
+                                    Buy Now
                                     <svg
                                         className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                                         aria-hidden="true"
