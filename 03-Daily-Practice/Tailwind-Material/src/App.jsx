f16b16c897bbb1c5389f0fdec04a5254ebbb5e6a
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-12 23:08:02 +0500

    Practice

---
+++ 03-Daily-Practice/Tailwind-Material/src/App.jsx
@@ -0,0 +1,16 @@
+import react from 'react'
+import App_bar from './Components/App_bar'
+
+function App() {
+
+  return (
+    <>
+      <App_bar />
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
