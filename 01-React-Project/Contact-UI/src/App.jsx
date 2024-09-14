a95555a21c956e854f9864545d23c8352eb590e9
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-14 21:01:43 +0500

    Complete

--- 01-React-Project/Contact-UI/src/App.jsx
+++ 01-React-Project/Contact-UI/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar_img from './assets/Frame 2 1.png';
 import Start from './Components/Start';
 import Inputs_img from './Components/Inputs_img';
+import Navbar from './Components/Navbar';
 
 function App() {
 
   return (
     <>
+    <Navbar />
       {/* Content */}
       <Start />
       <Inputs_img />
