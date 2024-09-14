82c9e838386d1c8ce6643bd753b3e5bb8158a231
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-14 19:45:36 +0500

    Complete

--- 01-React-Project/Contact-UI/src/Components/Inputs_img.jsx
+++ 01-React-Project/Contact-UI/src/Components/Inputs_img.jsx
@@ -3,119 +3,149 @@ import MessageIcon from '@mui/icons-material/Message';
 import PhoneIcon from '@mui/icons-material/Phone';
 import { Box, TextField, Button, Stack } from '@mui/material';
 import { Block } from '@mui/icons-material';
+import Center_img from '../assets/Service 24_7-pana 1.svg'
 
 const Inputs_img = () => {
   return (
     <>
+      <div className='flex flex-wrap justify-between items-center flex-wrap-reverse'>
+        <div>
+          <div>
+            <Stack spacing={2} direction="row" justifyContent="start" sx={{
+              marginTop: '2rem',
+              marginLeft: { xs: '1.25rem', sm: '13rem' }
+            }}>
+              {/* VIA SUPPORT CHAT Button */}
+              <Button
+                sx={{
+                  fontWeight: '600',
+                  marginLeft: '14rem',
+                  marginTop: '2.5rem',
+                  fontSize: { xs: '0.7rem', sm: '0.8rem' },
+                  padding: '0.7rem',
+                  width: { xs: '10rem', sm: '12rem' },
+                  borderRadius: '8px',
+                  color: 'white',
+                  backgroundColor: 'black',
+                  '&:hover': { backgroundColor: '#4B5563' }, // hover:bg-gray-800
+                  transition: 'background-color 0.13s',
+                }}
+              >
+                <MessageIcon sx={{
+                  fontSize: { xs: '0.8rem', sm: '1rem' },
+                  marginRight: '4px',
+                }} />  VIA SUPPORT CHAT
+              </Button>
 
-      <Stack spacing={2} direction="row" justifyContent="start" sx={{
-        marginTop: '2rem',
-        marginLeft: { xs: '1.25rem', sm: '13rem' }
-      }}>
-        {/* VIA SUPPORT CHAT Button */}
-        <Button
-          sx={{
-            fontWeight: '600',
-            marginLeft: '14rem',
-            marginTop: '2.5rem',
-            fontSize: '0.8rem',
-            padding: '0.7rem',
-            width: '12rem',
-            borderRadius: '8px',
-            color: 'white',
-            backgroundColor: 'black',
-            '&:hover': { backgroundColor: '#4B5563' }, // hover:bg-gray-800
-            transition: 'background-color 0.13s',
-          }}
-        >
-          <MessageIcon />  VIA SUPPORT CHAT
-        </Button>
+              {/* VIA CALL Button */}
+              <Button
+                sx={{
+                  fontWeight: '600',
+                  marginLeft: '1.25rem',
+                  marginRight: '1.25rem',
+                  marginTop: '2.5rem',
+                  fontSize: { xs: '0.7rem', sm: '0.8rem' },
+                  padding: '0.5rem 2rem',
+                  width: { xs: '10rem', sm: '12rem' },
+                  borderRadius: '8px',
+                  color: 'white',
+                  backgroundColor: 'black',
+                  '&:hover': { backgroundColor: '#4B5563' }, // hover:bg-gray-800
+                  transition: 'background-color 0.13s',
+                }}
+              >
+                <PhoneIcon sx={{
+                  fontSize: { xs: '0.8rem', sm: '1rem' },
+                  marginRight: '4px',
+                }} /> VIA CALL
+              </Button>
+            </Stack>
 
-        {/* VIA CALL Button */}
-        <Button
-          sx={{
-            fontWeight: '600',
-            marginLeft: '1.25rem',
-            marginRight: '1.25rem',
-            marginTop: '2.5rem',
-            fontSize: '0.8rem',
-            padding: '0.5rem 2rem',
-            width: '12rem',
-            borderRadius: '8px',
-            color: 'white',
-            backgroundColor: 'black',
-            '&:hover': { backgroundColor: '#4B5563' }, // hover:bg-gray-800
-            transition: 'background-color 0.13s',
-          }}
-        >
-          <PhoneIcon /> VIA CALL
-        </Button>
-      </Stack>
-
-      {/* VIA EMAIL CHAT Button */}
-      <Button
-        variant="contained"
-        type="submit"
-        sx={{
-          display: 'block',
-          fontWeight: '600',
-          marginLeft: { xs: '1.25rem', sm: '13rem' },
-          marginTop: '0.5rem',
-          padding: '0.5rem',
-          width: { xs: '23rem', sm: '25rem' },
-          borderRadius: '8px',
-          border: '1px solid black',
-          backgroundColor: 'white',
-          color: 'black',
-          '&:hover': {
-            backgroundColor: '#000', // hover:bg-gray-800
-            color: 'white',
-          },
-          transition: 'all 0.13s',
-        }}
-      >
-        <MessageIcon /> VIA EMAIL FORM
-      </Button>
+            {/* VIA EMAIL CHAT Button */}
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{
+                display: 'block',
+                fontWeight: '600',
+                marginLeft: { xs: '1.25rem', sm: '13rem' },
+                marginTop: '0.5rem',
+                fontSize: { xs: '0.7rem', sm: '0.8rem' },
+                padding: '0.5rem',
+                width: { xs: '21rem', sm: '25rem' },
+                borderRadius: '8px',
+                border: '1px solid black',
+                backgroundColor: 'white',
+                color: 'black',
+                '&:hover': {
+                  backgroundColor: '#000', // hover:bg-gray-800
+                  color: 'white',
+                },
+                transition: 'all 0.13s',
+              }}
+            >
+              <MessageIcon sx={{
+                fontSize: { xs: '0.8rem', sm: '1rem' },
+                marginRight: '4px',
+              }} /> VIA EMAIL FORM
+            </Button>
+          </div>
 
 
-      {/* TextField Form */}
+          {/* TextField Form */}
 
-      {/* Input Form */}
-      {/* <Box
-        // component="form"
-        sx={{
-          display: 'flex',
-          marginTop: '20px',
-          flexDirection: 'column',
-          width: '100%',
-          maxWidth: '300px',
-          '& .MuiTextField-root': { marginBottom: '1.5rem' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField label="Name" variant="outlined" fullWidth />
-        <TextField label="E-Mail" variant="outlined" fullWidth />
-        <TextField
-          label="TEXT"
-          variant="outlined"
-          multiline
-          rows={4}
-          fullWidth
-        />
-        <Button
-          variant="contained"
-          type="submit"
-          sx={{
-            backgroundColor: 'black',
-            color: 'white',
-            '&:hover': { backgroundColor: '#333' },
-            padding: '10px',
-          }}
-        >
-          SUBMIT
-        </Button>
-      </Box> */}
+          {/* Input Form */}
+          <div>
+            <Box
+              // component="form"
+              sx={{
+                display: 'flex',
+                marginTop: '20px',
+                marginLeft: { xs: '1.5rem', sm: '13rem' },
+                flexDirection: 'column',
+                width: '100%',
+                maxWidth: '300px',
+                '& .MuiTextField-root': { marginBottom: '1.5rem' },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField sx={{
+                width: { xs: '21rem', sm: '25rem' },
+              }} label="Name" variant="outlined" fullWidth />
+              <TextField sx={{
+                width: { xs: '21rem', sm: '25rem' },
+              }} label="E-Mail" variant="outlined" fullWidth />
+              <TextField sx={{
+                width: { xs: '21rem', sm: '25rem' },
+              }}
+                label="TEXT"
+                variant="outlined"
+                multiline
+                rows={4}
+                fullWidth
+              />
+            </Box>
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{
+                backgroundColor: 'black',
+                color: 'white',
+                marginBottom:'10px',
+                fontSize: { xs: '0.7rem', sm: '0.8rem' },
+                width: '12rem',
+                marginLeft: { xs: '1.5rem', sm: '26rem' },
+                '&:hover': { backgroundColor: '#333' },
+                padding: '10px',
+              }}
+            >
+              SUBMIT
+            </Button>
+          </div>
+        </div>
+        <img className='w-[90%] sm:w-[40%] mr-20' src={Center_img} alt="no img" />
+      </div>
     </>
   )
 }
--- 01-React-Project/Contact-UI/src/Components/Start.jsx
+++ 01-React-Project/Contact-UI/src/Components/Start.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Start = () => {
   return (
     <div>
-      <h1 className='poppins-bold sm:ml-52 ml-5  mr-5 mt-3 sm:mt-10 text-[2rem]'>CONTACT US</h1>
+      <h1 className='poppins-bold sm:ml-52 ml-5  mr-5 mt-3 sm:mt-10 text-[2rem]  sm:text-[3rem]'>CONTACT US</h1>
       <p className=' poppins-semibold sm:ml-52 ml-5 md:mr-48 sm:mr-16 mr-3  mt-1'>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
     </div>
   )
