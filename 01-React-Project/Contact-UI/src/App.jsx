ceb8f770ff3d2974a74353258c65f1ef977f3691
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-14 19:47:49 +0500

    Complete

--- 01-React-Project/Contact-UI/src/App.jsx
+++ 01-React-Project/Contact-UI/src/App.jsx
@@ -7,34 +7,6 @@ function App() {
 
   return (
     <>
-      {/* Navbar */}
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <img className="mt-1 sm:ml-20 ml-3" src={Navbar_img} alt="no img" />
-        </div>
-
-        {/* Menu for larger screens */}
-        <div className="hidden sm:flex flex-none mr-5">
-          <ul className="menu menu-horizontal px-1">
-            <li><a className="uppercase font-bold">Home</a></li>
-            <li><a className="uppercase font-bold">About</a></li>
-            <li><a className="uppercase font-bold">Contact</a></li>
-          </ul>
-        </div>
-
-        {/* Dropdown menu for smaller screens */}
-        <div className="sm:hidden flex-none mr-5">
-          <div className="dropdown dropdown-end">
-            <button tabIndex={0} className="btn m-1">Menu</button>
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box w-52 p-2 shadow">
-              <li><a className="uppercase font-bold">Home</a></li>
-              <li><a className="uppercase font-bold">About</a></li>
-              <li><a className="uppercase font-bold">Contact</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
       {/* Content */}
       <Start />
       <Inputs_img />
