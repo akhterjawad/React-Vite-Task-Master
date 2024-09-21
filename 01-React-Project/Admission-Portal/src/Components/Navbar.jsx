af81aaad39242aab3ba428b95de1da5f8ce8355e
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-21 16:47:04 +0500

    admission portal complete

--- 01-React-Project/Admission-Portal/src/Components/Navbar.jsx
+++ 01-React-Project/Admission-Portal/src/Components/Navbar.jsx
@@ -3,11 +3,14 @@ import ThemeSwitcher from './ThemeSwitcher'
 
 
 const Navbar = () => {
+  function reloader() {
+    window.location.reload()
+  }
   return (
     <>
       <div className="navbar bg-[#4a00ff] mb-4 sm:mb-6">
         <div className="flex-1">
-          <a className="btn btn-ghost text-[1.1rem] sm:text-xl">Innovatrix Academy</a>
+          <p onClick={reloader} className="btn btn-ghost text-[1.1rem] sm:text-xl">Innovatrix Academy</p>
         </div>
         <div className="text-center btn-ghost rounded-full w-[42px] h-[42px]">
           < ThemeSwitcher/>
