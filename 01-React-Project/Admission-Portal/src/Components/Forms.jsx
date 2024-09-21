6845e6ada6a4855ebd1aedc8054d81c9c658b08b
Muhammad Jawad <akhterjawad566@gmail.com>
2024-09-21 16:39:55 +0500

    admission portal complete

--- 01-React-Project/Admission-Portal/src/Components/Forms.jsx
+++ 01-React-Project/Admission-Portal/src/Components/Forms.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
 const Forms = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
     return (
         <>
             <h1 className="text-center font-bold text-[2rem]">Admission Form</h1>
-            <form className="p-8 sm:pl-16 sm:pr-16 ">
+            <form onSubmit={handleSubmit(onSubmit)} className="p-8 sm:pl-16 sm:pr-16 ">
                 <div className="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
                         <label
@@ -13,13 +22,14 @@ const Forms = () => {
                         >
                             Full name
                         </label>
-                        <input
+                        <input {...register("Full_name", { required: true })}
                             type="text"
                             id="Full_name"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Full name"
                             required=""
                         />
+                        {errors.Full_name && <span className='text-[red]'>This field is required</span>}
                     </div>
                     <div>
                         <label
@@ -28,13 +38,14 @@ const Forms = () => {
                         >
                             Father Name
                         </label>
-                        <input
+                        <input {...register("Father_Name", { required: true })}
                             type="text"
                             id="Father_Name"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Father Name"
                             required=""
                         />
+                        {errors.Father_Name && <span className='text-[red]'>This field is required</span>}
                     </div>
                     <div>
                         <label
@@ -43,13 +54,14 @@ const Forms = () => {
                         >
                             Email
                         </label>
-                        <input
-                            type="text"
+                        <input {...register("Email", { required: true })}
+                            type="email"
                             id="Email"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Email"
                             required=""
                         />
+                        {errors.Email && <span className='text-[red]'>This field is required</span>}
                     </div>
                     <div>
                         <label
@@ -58,14 +70,15 @@ const Forms = () => {
                         >
                             Phone number
                         </label>
-                        <input
-                            type="tel"
+                        <input {...register("phone_number", { required: true })}
+                            type="number"
                             id="phone_number"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Enter first digits is 0"
                             pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                             required=""
                         />
+                        {errors.phone_number && <span className='text-[red]'>This field is required</span>}
                     </div>
                     <div>
                         <label
@@ -74,13 +87,14 @@ const Forms = () => {
                         >
                             National ID (CNIC)
                         </label>
-                        <input
-                            type="url"
+                        <input {...register("CNIC", { required: true })}
+                            type="number"
                             id="CNIC"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="CNIC"
                             required=""
                         />
+                        {errors.CNIC && <span className='text-[red]'>This field is required</span>}
                     </div>
                     <div>
                         <label
@@ -89,13 +103,14 @@ const Forms = () => {
                         >
                             Date of Birth
                         </label>
-                        <input
+                        <input {...register("Date_of_Birth", { required: true })}
                             type="date"
                             id="Date_of_Birth"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder=""
                             required=""
                         />
+                        {errors.Date_of_Birth && <span className='text-[red]'>This field is required</span>}
                     </div>
                 </div>
                 <div className="mb-6">
@@ -106,10 +121,11 @@ const Forms = () => {
                         Gender
                     </label>
 
-                    <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="Gender" name="gender">
+                    <select {...register("Gender", { required: true })} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="Gender" name="gender">
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                     </select>
+                    {errors.Gender && <span className='text-[red]'>This field is required</span>}
                 </div>
                 <div className="mb-6">
                     <label
@@ -119,11 +135,12 @@ const Forms = () => {
                         Do you have a Laptop?
                     </label>
 
-                    <select
+                    <select {...register("Laptop", { required: true })}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="gender" name="gender">
                         <option value="Yes">yes</option>
                         <option value="No">No</option>
                     </select>
+                    {errors.Laptop && <span className='text-[red]'>This field is required</span>}
                 </div>
                 <div className="mb-6">
                     <label
@@ -132,13 +149,14 @@ const Forms = () => {
                     >
                         Address
                     </label>
-                    <input
+                    <input {...register("Address", { required: true })}
                         type="text"
                         id="Address"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Address"
                         required=""
                     />
+                    {errors.Address && <span className='text-[red]'>This field is required</span>}
                 </div>
                 <div className="mb-6">
                     <label
@@ -147,23 +165,25 @@ const Forms = () => {
                     >
                         Last qualification
                     </label>
-                    <select
+                    <select  {...register("qualification", { required: true })}
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="qualification" name="gender">
                         <option value="Matric">Matric</option>
                         <option value="Intermeiate">Intermeiate</option>
                         <option value="Bachelors">Bachelors</option>
                         <option value="Masters">Masters</option>
                     </select>
+                    {errors.qualification && <span className='text-[red]'>This field is required</span>}
                 </div>
                 {/* <div className="flex items-start mb-6">
            <div className="flex items-center h-5">
-            <input
+            <input {...register("email", { required: true })}
               id="remember"
               type="checkbox"
               defaultValue=""
               className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
               required=""
             />
+            {errors.email && <span className='text-[red]'>This field is required</span>}
           </div> 
            <label
             htmlFor="remember"
@@ -183,7 +203,8 @@ const Forms = () => {
                     >
                         Profile Image
                     </label>
-                    <input type="file" id="Profile_Image" className="file-input file-input-bordered w-full max-w-xs mb-5" />
+                    <input {...register("Profile_Image", { required: true })} type="file" id="Profile_Image" className="file-input file-input-bordered w-full max-w-xs mb-1" /> <br />
+                    {errors.Profile_Image && <span className='text-[red] mb-3'>This field is required</span>}
                 </div>
                 <ul className="list-disc pl-5 mb-5">
                     <li>With white or blue background</li>
--- 02-Class-Work/06-React-Hook-Form/src/App.jsx
+++ 02-Class-Work/06-React-Hook-Form/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useForm } from "react-hook-form"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" placeholder='enter your email' {...register("email", { required: true })} /><br />
+        {errors.email && <span className='text-danger'>This field is required</span>}
+        <br />
+        <input type="password" placeholder='enter your password' {...register("password", { required: true })} /><br />
+        {errors.password && <span className='text-danger'>This field is required</span>}
+        <br />
+        <button type='submit'>login</button>
+      </form>
+
     </>
   )
 }
